93f5bb448d7b83bbb278010f3153135c1ed1054e
Byoungcheon Lee <lbcsultan@gmail.com>
2024-06-12 10:34:38 +0900

    2nd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="container">
-      <h1>My Next.js Blog</h1>
+      <h1 className="text-2xl font-bold">My Next.js Blog</h1>
+      <div className="mt-4 flex flex-col gap-4">
+        <Image src="/blog.jpg" alt="blog" width="700" height="500" />
+        <Image src="/blog2.jpg" alt="blog" width="700" height="500" />
+      </div>
     </div>
   )
 }
